d1c10b53841bd000d68872442e361007f1aa9750
santhisenan <santhisenan@outlook.com>
2018-03-11 19:22:49 +0530

    exp9

--- exp9/client.py
+++ exp9/client.py
@@ -4,9 +4,7 @@ import select
 import sys
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-if len(sys.argv) != 3:
-    print "Usage: script, IP address, port"
-    # exit()
+
 
 IP_ADDRESS = "127.0.0.1"
 PORT = 8000
@@ -15,16 +13,14 @@ server.connect((IP_ADDRESS, PORT))
 while True:
     sockets = [sys.stdin, server]
     read_sockets, write_socket, error_socket = select.select(sockets,[],[])
-
-    for socks in read_sockets:
-        if socket in read_sockets:
-            if socket == server:
-                message = socket.recv(2048)
-                print message
-            else:
-                message = sys.stdin.readline()
-                server.send(message)
-                sys.stdout.write("<You>")
-                sys.stdout.write(message)
-                sys.stdout.flush()
+    for sock in read_sockets:
+        if sock == server:
+            message = sock.recv(2048)
+            print message
+        else:
+            message = sys.stdin.readline()
+            server.send(message)
+            sys.stdout.write("<You>")
+            sys.stdout.write(message)
+            sys.stdout.flush()
 server.close()                    
--- exp9/server.py
+++ exp9/server.py
@@ -5,55 +5,55 @@ import select # to provide accessto the select() and poll() functions available 
 import sys # system specific parameters and function
 from thread import * # provides low-level primitives for working with multiple threads
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # server socket in udp
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # server socket in tcp
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 
-#check if number of args are 3
-if len(sys.argv) != 3:
-    print "Number of arguments is not 3."
-    # print "Usage: script, IP Address, Port Number"
-    # exit()
 
-IP_ADDR = "127.0.0.1" # first arg
-PORT = 8000 # second arg
+IP_ADDR = "127.0.0.1"
+PORT = 8000 
 server.bind((IP_ADDR, PORT))
 
 server.listen(100)
 
 clients = [] # list of clients
 
-def clientThread(conn, addr):
-    conn.send("Hi there!")
-
-    while True:
-        try:
-            message = conn.recv(2048)
-            if message:
-                print "[" + addr[0] + "] "+ message
-                brodcastMessage = "[" + addr[0] + "] "+ message
-                broadcast(brodcastMessage, conn)
-            else: 
-                remove(conn)
-        except: 
-            continue
-def broadcast(message, connection):
-    for client in clients:
-        if(client != connection):
-            try:
-                client.send(message)
-            except:
-                client.close()
-
-                remove(client)
 
 def remove(connection):
     if connection in clients:
         clients.remove(connection)
 
+def broadcast(message, connection):
+    for client in clients:
+        if client != connection:
+            try:
+                client.send(message)
+                break
+            except:
+                client.close()
+                
+                remove(client)
+
+
+def clientThread(conn, addr):
+    conn.sendto("Hi there, ~from server..!", addr)
+
+    while True:
+        try:
+            message = conn.recv(2048)
+            if message:
+                print "[" + addr[0] + "]: "+ str(addr[1]) + " => "+ message
+                broadcastMessage = "<[" + addr[0] + "] :" + str(addr[1]) + "> => "+ message
+                broadcast(broadcastMessage, conn)
+            else: 
+                remove(conn)
+        except: 
+            continue
+
+
 while True:
     conn, addr = server.accept()
     clients.append(conn)
-    print addr[0] + " connected"
+    print addr[0] + ":"+ str(addr[1])+ " connected"
     start_new_thread(clientThread,(conn,addr)) 
 conn.close()
 server.close()                               
