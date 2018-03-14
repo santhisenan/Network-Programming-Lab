815e0bc83ae554dd2d67376010de837762e3a48b
santhisenan <santhisenan@outlook.com>
2018-03-14 10:49:43 +0530

    SMTP complete

--- exp13/client.py
+++ exp13/client.py
@@ -13,41 +13,51 @@ def printMessageFromServer():
 
 printMessageFromServer()
 
-request = input('... Enter HELO: <domain-name> ... \n')
+print ('... Enter HELO: <domain-name> ... \n')
+print('... Enter MAIL FROM: <mail-address-of-sender> ... \n')
+print('... Enter RCPT TO: <mail-address-of-recipient> ... \n')
+print('... Enter DATA ... \n')
+print('... Enter the contents of the message ...')
+print('... Enter QUIT ... \n')
+request = input()
 if (request[:4] == 'HELO'):
     clientSocket.send(request.encode())
 printMessageFromServer()
 
-request = input('... Enter MAIL FROM: <mail-address-of-sender> ... \n')
+request = input()
 if (request[:9] == 'MAIL FROM'):
     clientSocket.send(request.encode())
 printMessageFromServer()
 
 
-request = input('... Enter RCPT TO: <mail-address-of-recipient> ... \n')
+request = input()
 if (request[:7] == 'RCPT TO'):
     clientSocket.send(request.encode())
 printMessageFromServer()
 
-request = input('... Enter DATA ... \n')
+request = input()
 if (request[:4] == 'DATA'):
     clientSocket.send(request.encode())
 printMessageFromServer()
 
-print('... Enter the contents of the message ...')
+print()
 delim = ""
 request = ""
 data = []
 while True:
     line = input()
-    if line[-1:] == '.':
+    if line == '.':
         break;
     else:
         data.append(line)
 
-request = '\n.'.join(data)
-
-print (request)
-clientSocket.send(request)
+request = '\n'.join(data)
+clientSocket.send(request.encode())
 printMessageFromServer()
 
+request = input()
+if (request[:4] == 'QUIT'):
+    clientSocket.send(request.encode())
+printMessageFromServer()
+
+clientSocket.close()
--- exp13/server.py
+++ exp13/server.py
@@ -32,6 +32,21 @@ while True:
   
     request = conn.recv(1024).decode()
     if(request[:4] == 'DATA'):
-        conn.send('354: Start mail input'.encode())   
+        conn.send('354: Start mail input'.encode())  
 
+
+    request = conn.recv(1024).decode()
+    email_data = request[:]
+    conn.send('250: OK'.encode())  
+    
+
+    
+    request = conn.recv(1024).decode()
+    if(request[:4] == 'QUIT'):
+        conn.send('221: Service closing transmission channel'.encode()) 
+
+    conn.close()
+    break
+
+serverSocket.close()
 
